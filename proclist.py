1b237e80b7e966d8aa03cda54cd558aec328750c
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-06-23 14:03:04 +0000

    Update proclist.py

--- proclist.py
+++ proclist.py
@@ -18,3 +18,14 @@ for j in pid:
     cmd = read_cmdline("/proc/" + str(j) + "/cmdline")
     if cmd is not 0:
         print (cmd[0:cmd.find("--")], j)
+
+def status(pid, dic={}):
+    file = "/proc/"+str(pid)+"/status"
+    with open(file) as status:
+        beta = status.read()
+    for j in beta.splitlines():
+        key, value = j.split("\t")[0][0:-1], j.split("\t")[1]
+        dic[key] = {"value":value.strip()}
+    return dic
+
+print (status(1))
