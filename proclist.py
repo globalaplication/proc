6c7aada3a44742015dc05f788b0ebd8bee3b78e3
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2018-06-23 16:42:29 +0000

    Update proclist.py

--- proclist.py
+++ proclist.py
@@ -28,3 +28,31 @@ def status(pid, dic={}):
 for pid in psutil.pids():
     dict = status(pid)
     print(pid, dict["cmd"], dict["Name"]["value"], dict["Umask"]["value"])
+
+    
+    -----------
+    
+    
+    import psutil
+def read_cmdline(file):
+    with open(file) as read:
+        beta = read.read()
+    if len(beta) is 0:
+        return 0
+    else:
+        return beta
+def status(pid, dic={}):
+    file = "/proc/"+str(pid)+"/status"
+    cmd = read_cmdline("/proc/" + str(pid) + "/cmdline")
+    if cmd is not 0:
+        cmd = cmd[0:cmd.find("--")]
+    with open(file) as status:
+        beta = status.read()
+    for j in beta.splitlines():
+        key, value = j.split("\t")[0][0:-1], j.split("\t")[1]
+        dic[key] = {"cmd":cmd, "value":value.strip()}
+    return dic
+
+for pid in psutil.pids():
+    dict = status(pid)
+    print(pid,  dict["Name"]["value"], dict["Umask"]["value"])
